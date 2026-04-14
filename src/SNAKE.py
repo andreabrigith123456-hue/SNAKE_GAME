6b742f852191526fed426f3410b4ba1cd1ece10c
Andrea Pumisacho <andrea123@gmail.com>
2026-04-14 17:08:28 -0500

    mi proyecto

---
+++ src/SNAKE.py
@@ -0,0 +1,51 @@
+import pygame, sys, time, random
+
+pygame.init()
+
+play_surface = pygame.display.set_mode((500, 500))
+
+font = pygame.font.Font(None, 30)
+
+fps = pygame.time.Clock()
+
+
+def food():
+    random_pos = random.randint(0,49)*10
+    food_pos = [random_pos, random_pos]
+    return food_pos
+
+
+def main():
+
+    snake_pos = [100, 50]
+    snake_body = [[100,50],[90,50],[80,50]]
+    change = "RIGHT"
+    run = True
+    food_pos = food()
+    score = 0
+
+    while run:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    change = "RIGHT"
+                if event.key == pygame.K_LEFT:
+                    change = "LEFT"
+                if event.key == pygame.K_UP:
+                    change = "UP"
+                if event.key == pygame.K_DOWN:
+                    change = "DOWN"
+        if change == "RIGHT":
+            snake_pos[0] += 10
+        if change == "LEFT":
+            snake_pos[0] -= 10
+        if change == "UP":
+            snake_pos[1] -= 10
+        if change == "DOWN":
+            snake_pos[1] += 10
+
+
+        
